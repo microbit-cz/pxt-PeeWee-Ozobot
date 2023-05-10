8700f914e85ea1e43ca30ba4133bc6d16ad61165
Antonín Reiser <115144153+kompetetivnimarxista@users.noreply.github.com>
2023-05-10 21:58:48 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,20 @@
+//M1 levy motor 
+//M4 pravy motor 
 
-basic.clearScreen()
+const centerP: DigitalPin = DigitalPin.P15;
+let p1: number = 0
+basic.forever(function() {
+    p1 = pins.digitalReadPin(centerP)
+    console.log(p1)
+    basic.pause(10) 
+})
 
 input.onButtonPressed(Button.A, function() {
-    PCAmotor.MotorRun(PCAmotor.Motors.M1, 250)
-    PCAmotor.MotorRun(PCAmotor.Motors.M4, -250)
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, 90) 
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, 90)
 })
+
 
 input.onButtonPressed(Button.B, function () {
     PCAmotor.MotorStopAll()
-})
+});;
