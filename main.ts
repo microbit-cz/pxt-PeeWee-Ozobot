7d4adfbde98c0f3fd20ccc2e9aa1a58f7e9614e6
Antonín Reiser <115144153+kompetetivnimarxista@users.noreply.github.com>
2023-05-31 21:50:40 +0200

    dying

--- main.ts
+++ main.ts
@@ -11,17 +11,39 @@ const leftP: DigitalPin = DigitalPin.P14;
 let center: number = 0
 let right: number = 0
 let left: number = 0
-let speed: number = 75
-let speedTurn: number = 65
-let turnRight = false
-let turnLeft = false
-let time1 = 0
-let time2 = 0
+let speed1: number = 70
+let speed2: number = 70
+
+let time = 0
 pins.setPull(centerP, PinPullMode.PullNone)
 pins.setPull(rightP, PinPullMode.PullNone)
 pins.setPull(leftP, PinPullMode.PullNone)
 
-input.onButtonPressed(Button.A, function () {
+function turnLeft() {
+    // if (center == 1) {
+        time = control.millis()
+        speed2 = 67
+        if (control.millis() == time + 20) {
+        time = 0
+        speed1 = 70
+        speed1 = 70
+        }
+    // }
+    
+}
+function turnRight() {
+    // if(center == 1) {
+        time = control.millis()
+        speed1 = 67
+        if (control.millis() == time + 20) {
+        time = 0
+        speed1 = 70
+        speed2 = 70
+        }
+    // }
+    
+}
+
     basic.forever(function () {
         center = pins.digitalReadPin(centerP);
         right = pins.digitalReadPin(rightP);
@@ -32,9 +54,8 @@ input.onButtonPressed(Button.A, function () {
         // basic.pause(50)
         if (center == 1) {
             if (left == 0 && right == 0) {
-                speed = 70
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
+                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
                 basic.showLeds(`
                     . . # . .
                     . # # # .
@@ -46,10 +67,10 @@ input.onButtonPressed(Button.A, function () {
         }
         // if (center == 0) {
             if (right == 0 && left == 1){
-                speed = 70
-                speedTurn = 50
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speedTurn)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
+                speed1 = 70
+                speed2 = 55
+                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed2)
+                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed1)
                 basic.showLeds(`
                     . . # . .
                     . # . . .
@@ -57,13 +78,15 @@ input.onButtonPressed(Button.A, function () {
                     . # . . .
                     . . # . .
                     `)
-                turnLeft = true
+                if(center == 1){
+                   turnLeft() 
+                }
             }
-            if (right == 1 && left == 0){
-                speed = 70
-                speedTurn = 50
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speedTurn)
+            else if (right == 1 && left == 0){
+                speed1 = 70
+                speed2 = 55
+                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
+                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
                 basic.showLeds(`
                     . . # . .
                     . . . # .
@@ -71,48 +94,12 @@ input.onButtonPressed(Button.A, function () {
                     . . . # .
                     . . # . .
                     `)
-                turnRight = true
-            }
-        // }
-        if (turnLeft == true){
-            time1 = control.millis()
-            if(center == 1){
-                time2 = control.millis()
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed - 10)
-                if (time2 - time1 > 50){
-                    time1 = 0
-                    time2 = 0
-                    turnLeft = false
+                if (center == 1){
+                    turnRight()
                 }
             }
-        }
-        if (turnRight == true) {
-            if (center == 1) {
-                time2 = control.millis()
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed - 10)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
-                if (time2 - time1 > 50) {
-                    time1 = 0
-                    time2 = 0
-                    turnRight = false
-                }
-            }
-        }
-
-        // if (center == 1 &&  left == 1){
-        //     PCAmotor.MotorRun(PCAmotor.Motors.M1, -75)
-        //     PCAmotor.MotorRun(PCAmotor.Motors.M4, speed)
-        // }   
-        // if (center == 1 && right == 1) {
-        //     PCAmotor.MotorRun(PCAmotor.Motors.M1, speed)
-        //     PCAmotor.MotorRun(PCAmotor.Motors.M4, -75)
         // }
-
-
-    
     })
-})
 
 
 
