f555d3ecf42d4fe89a7a3e26f5a4620ccf3c3ba3
Antonín Reiser <115144153+kompetetivnimarxista@users.noreply.github.com>
2023-06-01 17:44:32 +0200

    rah

--- main.ts
+++ main.ts
@@ -19,20 +19,16 @@ pins.setPull(centerP, PinPullMode.PullNone)
 pins.setPull(rightP, PinPullMode.PullNone)
 pins.setPull(leftP, PinPullMode.PullNone)
 
-function turnLeft() {
-    // if (center == 1) {
+function stableLeft() {
         time = control.millis()
         speed2 = 67
         if (control.millis() == time + 20) {
         time = 0
         speed1 = 70
-        speed1 = 70
+        speed2 = 70
         }
-    // }
-    
 }
-function turnRight() {
-    // if(center == 1) {
+function stableRight() {
         time = control.millis()
         speed1 = 67
         if (control.millis() == time + 20) {
@@ -40,9 +36,53 @@ function turnRight() {
         speed1 = 70
         speed2 = 70
         }
-    // }
-    
 }
+
+function forward(){
+    speed1 = 70
+    speed2 = 70
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+}
+
+function turnLeft(){
+    speed1 = 55
+    speed2 = 75
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
+    basic.showLeds(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `)
+    // if(center == 1){
+
+    // }
+}
+
+function turnRight() {
+    speed1 = 75
+    speed2 = 55
+    PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
+    PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `)
+}
+
 
     basic.forever(function () {
         center = pins.digitalReadPin(centerP);
@@ -54,49 +94,17 @@ function turnRight() {
         // basic.pause(50)
         if (center == 1) {
             if (left == 0 && right == 0) {
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
-                basic.showLeds(`
-                    . . # . .
-                    . # # # .
-                    # . # . #
-                    . . # . .
-                    . . # . .
-                    `)
+                forward()
             }
         }
         // if (center == 0) {
             if (right == 0 && left == 1){
-                speed1 = 70
-                speed2 = 55
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed2)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed1)
-                basic.showLeds(`
-                    . . # . .
-                    . # . . .
-                    # # # # #
-                    . # . . .
-                    . . # . .
-                    `)
-                if(center == 1){
-                   turnLeft() 
-                }
+                turnLeft() 
+                
             }
             else if (right == 1 && left == 0){
-                speed1 = 70
-                speed2 = 55
-                PCAmotor.MotorRun(PCAmotor.Motors.M1, speed1)
-                PCAmotor.MotorRun(PCAmotor.Motors.M4, speed2)
-                basic.showLeds(`
-                    . . # . .
-                    . . . # .
-                    # # # # #
-                    . . . # .
-                    . . # . .
-                    `)
-                if (center == 1){
-                    turnRight()
-                }
+               turnRight()
+            
             }
         // }
     })
